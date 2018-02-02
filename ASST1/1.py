deabc3ec5892e4266bda3b814772d1530d509ee2
Michael Vu <michael.vu.97@outlook.com>
2018-02-01 22:30:44 -0500

    Cleaned up 1.py

--- ASST1/1.py
+++ ASST1/1.py
@@ -1,19 +1,21 @@
 import numpy as np
 import tensorflow as tf
 
-
+"""
+Returns a Pairwise Euclidian distance matrix
+"""
 def PairwiseEuclidian(X,Y):
     return tf.reduce_sum((tf.expand_dims(X, 1) - tf.expand_dims(Z, 0))**2, 2)
 
 ################################################################################
 
-
 init = tf.global_variables_initializer()
-
 
 sess = tf.InteractiveSession()
 sess.run(init)
+################################################################################
 
+# Test Tensors
 X = tf.constant([[1,2],[3,4]])
 Z = tf.constant([[1,2],[3,4],[5,6]])
 
