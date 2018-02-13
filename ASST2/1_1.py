0523c069f03f7cee7dc33e9a6124b295477a6b2a
MertTens <amertens114@gmail.com>
2018-02-13 00:19:55 -0500

    Part 1.1 complete

--- ASST2/1_1.py
+++ ASST2/1_1.py
@@ -1,4 +1,6 @@
-
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 # The given code to get the data
 
@@ -18,3 +20,78 @@ with np.load("notMNIST.npz") as data :
     trainData, trainTarget = Data[:3500], Target[:3500]
     validData, validTarget = Data[3500:3600], Target[3500:3600]
     testData, testTarget = Data[3600:], Target[3600:]
+
+
+# Given parameters
+# One iteration is to run through an entire mini batch (size 500 here)
+# Note that one epoch is one runthrough of the entire training data set
+# This means that we will be training for about 2857 epochs
+# We should be plotting training loss function per # epochs, meaning one entry every
+# 7 iterations
+trainDataSize = 3500
+batchSize = 500
+numIterations = 20000
+learningRates = [.005, .001, .0001]
+
+# Input and output data
+X = tf.placeholder(tf.float64)
+Y = tf.placeholder(tf.float64)
+
+
+for rate in learningRates:
+
+    # Weights
+    w = tf.expand_dims(tf.Variable(tf.zeros([784], dtype=tf.float64), name="weights"), 1)
+    b = tf.Variable(tf.zeros([1], dtype = tf.float64), name = "bias")
+
+    # Reshape the input so that each picture is a vector rather than a matrix
+    trainData = np.reshape(trainData, [3500, -1])
+
+    # Create a prediction function
+    yhat = tf.add(tf.matmul(X, w), b)
+
+    # The function to minimize
+    MSE = tf.reduce_sum(((yhat - Y)**2)/(2*trainDataSize))
+
+    # I needed to re create the variables. The easiest way to do this seemed to be to
+    # start a new session for each time I recreated the variables
+    ################################################################################
+    init = tf.global_variables_initializer()
+
+    sess = tf.InteractiveSession()
+    sess.run(init)
+    ################################################################################
+    
+    # Where each batch begins in the training data
+    startPoint = 0
+
+    # Create the optimizer. Every time a session is run, 
+    optimizer = tf.train.GradientDescentOptimizer(rate).minimize(MSE)
+
+    # The losses at each epoch and the epoch itself
+    epochLoss = []
+    epoch = np.arange(2857)
+
+    for iteration in range(numIterations):
+
+        # Run the optimizer
+        sess.run(optimizer, feed_dict={X: trainData[startPoint : startPoint + batchSize], Y: trainTarget[startPoint : startPoint + batchSize]})
+        
+        # Update the start of the batch as needed
+        startPoint = (startPoint + batchSize) % trainDataSize
+    
+        # If an epoch has completed, then update the epoch loss
+        if((iteration + 1)%7 == 0):
+            tempMSE = sess.run(MSE, feed_dict={X: trainData, Y: trainTarget})
+            #print("MSE: ", tempMSE)
+            epochLoss.append(tempMSE)
+    
+    # After optimizing, plot the epoch and the loss
+    epochLoss = np.array(epochLoss)
+    label = "loss with training rate " + str(rate)
+    plt.plot(epoch, epochLoss, label=label)
+
+
+# Create the plot
+plt.legend()
+plt.show()
