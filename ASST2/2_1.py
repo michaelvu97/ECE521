47c02d95c7f8f45ebb5bc4aa78e7b722032e4520
Michael Vu <michael.vu.97@outlook.com>
2018-02-21 14:58:55 -0500

    primitive setup for 2_1

--- ASST2/2_1.py
+++ ASST2/2_1.py
@@ -1,9 +1,36 @@
+import tensorflow as tf
+import numpy as np
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+
 # Basic Logistic Regresion Model
 
 X = tf.placeholder(tf.float64); # [Nxd]
-Y = tf.placeholder(tf.float64); # [Nx1]
+Y = tf.placeholder(tf.float64); # [N]
 
-w # TODO, [d]
-b # TODO, [1]
+one = tf.constant([1], dtype=tf.float64)
 
-yHat = tf.sigmoid(tf.matmul);
+# w = tf.expand_dims(tf.Variable(tf.zeros([2], dtype=tf.float64)),1) # [d]
+# b = tf.Variable(tf.zeros([1], dtype=tf.float64)) # [1]
+w = tf.expand_dims(tf.constant([1,2], dtype=tf.float64),1) # [d]
+b = tf.constant([0], dtype=tf.float64) # [1]
+
+lambda_weight_penalty = tf.constant(0.01, dtype=tf.float64)
+
+# yhat = sigmoid(w^TX + b) in R^[N]
+yHat = tf.sigmoid(tf.add(tf.matmul(X, w), b))
+
+Loss_Data = \
+        tf.reduce_sum( \
+            tf.negative(tf.multiply(Y, tf.log(yHat))) - \
+            tf.multiply(one - Y, tf.log(one - yHat)) \
+        ) / tf.cast(tf.shape(Y)[0], tf.float64);
+
+Loss_Weights = (lambda_weight_penalty / tf.constant(2.0, dtype=tf.float64)) * tf.reduce_sum(w**2)
+
+Loss = Loss_Data + Loss_Weights
+
+print(sess.run(Loss, feed_dict={X:np.array([[1,2],[3,4]]), Y:np.array([1,-1])}))
